dc109a96e62acc09ac405e8573b584a4ee3879c9
Techiebud <techiebud@gmail.com>
2017-03-05 11:59:34 -0500

    Adding bootstrap and bootswatch.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  bcImagePath = '../assets/images/bordercollie.gif';
   title = 'Georgia Border Collie Rescue';
 }
