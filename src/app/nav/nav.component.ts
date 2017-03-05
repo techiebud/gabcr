199b78009a4a437e979e1e9d04725b9deeea4e69
Techiebud <techiebud@gmail.com>
2017-03-05 14:58:00 -0500

    Navigation Development

---
+++ src/app/nav/nav.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  // tslint:disable-next-line:component-selector
+  selector: 'app-nav',
+  templateUrl: './nav.component.html',
+  styleUrls: ['./nav.component.scss']
+})
+export class NavComponent implements OnInit {
+  bcImagePath = '../../assets/images/bordercollie.gif';
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
